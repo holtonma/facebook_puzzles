c925e5291cf065cffe1a48c86cbaa332489e305c
Mark Holton <holtonma@Macintosh-69.local>
2009-12-15 20:45:57 -0800

    progress..

--- liarliar/lib/liarliar.rb
+++ liarliar/lib/liarliar.rb
@@ -1,22 +1,39 @@
 
-module Hiphop
+module Liar
   class << self
 
-    def translate some_int
-      return "Hop" if (some_int % 5 == 0 && some_int % 3 == 0)
-      return "Hoppity" if (some_int % 3 == 0)
-      return "Hophop" if (some_int % 5 == 0)
-    end
-    
-    def iterate max_int
-      n = 1
-      while n <= max_int
-        puts "#{Hiphop.translate(n)}" if Hiphop.translate(n) != nil
-        n += 1
+    def categorize liar_data
+      # {:liars => [], :truthers => []} 
+      fake_liar_data = [ 
+        { :user => "Stephen", :accused => ["Tommaso"]          },
+        { :user => "Tommaso", :accused => ["Galileo"]          },
+        { :user => "Isaac"  , :accused => ["Tommaso"]          },
+        { :user => "Stephen", :accused => ["Tommaso"]          },
+        { :user => "Galileo", :accused => ["Tommaso"]          },
+        { :user => "George" , :accused => ["Isaac", "Stephen"] }
+      ]
+      
+      liar_data = fake_liar_data
+      
+      cat = {:liars = [], :truthers = []}
+      
+      liar_data.each do |m|
+        puts "m[:user]= #{m[:user]} : m[:accused]= #{m[:accused]}"
+        # first person accused is a liar
+        # first one of lowest number of accused by any person is a liar
+        
+        cat[:liars] << m[:user] if liars.length == 0
+        #if 
       end
+      
+      return "18 3"
     end
     
-    def read_max_from_file(file_path)
+    def assemble_data contents
+      puts "contents: #{contents}"
+    end
+    
+    def read_file(file_path)
       file = File.new(file_path, "r")
       return file.gets.chomp.to_i
     end
--- liarliar/test/test_liarliar.rb
+++ liarliar/test/test_liarliar.rb
@@ -1,28 +1,47 @@
 require 'test/unit'
-require 'hiphop'
+require 'liarliar'
 
-class TestHoppity < Test::Unit::TestCase
+class TestLiar < Test::Unit::TestCase
   
-  def test_input_divisible_by_three 
-    assert_equal("Hoppity", Hiphop.translate(3))
+  def setup
+    # output of reading file:
+    @liar_data = [ 
+      { :user => "Stephen", :accused => ["Tommaso"]          },
+      { :user => "Tommaso", :accused => ["Galileo"]          },
+      { :user => "Isaac"  , :accused => ["Tommaso"]          },
+      { :user => "Stephen", :accused => ["Tommaso"]          },
+      { :user => "Galileo", :accused => ["Tommaso"]          },
+      { :user => "George" , :accused => ["Isaac", "Stephen"] }
+    ]
   end
   
-  def test_input_divisible_by_five 
-    assert_equal("Hophop", Hiphop.translate(5))
-  end
-  
-  def test_input_divisible_by_three_and_five 
-    assert_equal("Hop", Hiphop.translate(15))
-  end
-  
-  def test_input_not_divisible_by_three_or_five 
-    assert_equal(nil, Hiphop.translate(62))
+  def test_array_equal
+    accused_1 = ["one", "two"]
+    accused_2 = ["one", "two"]
+    accused_3 = ["two", "three"]
+    accused_4 = ["one"]
+    
+    assert_equal(accused_1, accused_2)
+    assert_not_equal(accused_1, accused_3)
+    assert_not_equal(accused_1, accused_4)
   end
   
   def test_read_file
-    assert_equal(22, Hiphop.read_max_from_file("../testfile.txt"))
-    assert_equal(22, Hiphop.read_max_from_file("../testfile.rtf"))
+    assert_equal(false, Liar.read_file("../testfile.txt"))
   end
   
+  def test_categorize
+    file_contents = Liar.read_file("../testfile.txt")
+    assert_equal("4 2", Liar.categorize(file_contents))
+  end
+  
+  def test_assemble_data
+    file_contents = Liar.read_file("../testfile.txt")
+    assert_equal(false, Liar.assemble_data(file_contents))
+  end
+      
+  def test_analyze_liars 
+    assert_equal("4 3", Liar.analyze_liars(@liar_data))
+  end
   
 end
